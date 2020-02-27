ff81ee7fdbfbc9ae120a5ccaad3acc4fa22f32d2
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-02-27 09:38:07 +0900

    solve 769

--- 769.cpp
+++ 769.cpp
@@ -1,143 +1,73 @@
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <cmath>
-#include <ctime>
-#include <functional>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <stack>
-#include <string>
-#include <tuple>
-#include <vector>
-
+#include<iostream>
+#include<vector>
 using namespace std;
 
-int nextTurn(int turn, int d, int mod)
-{
-  turn += d;
-  if (turn < 0)
-  {
-    return turn + mod;
-  }
-  else
-  {
-    return turn % mod;
-  }
+int n, m;
+int turn, dir;
+
+void proceed() {
+  turn = (turn + dir + n) % n;
 }
 
-int main()
-{
-  int n, m;
+int main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
   cin >> n >> m;
-  string l[m];
-  int c[n];
-  for (int i = 0; i < m; i++)
-  {
-    cin >> l[i];
-  }
-  for (int i = 0; i < n; i++)
-  {
-    c[i] = 0;
-  }
 
-  int turn = 0;
-  int drawCardCnt = 0;
-  int drawNumBefore = 0; // 0 or 2 or 4
-  int d = 1;
+  vector<int> put(n, 0), draw(n, 0);
+  int d2 = 0, d4 = 0;
+  turn = 0, dir = 1;
 
-  for (int i = 0; i < m; i++)
-  {
-    string li = l[i];
-    int drawNum = -1;
+  string card;
+  while (m-- > 0) {
+    cin >> card;
 
-    if (li == "drawtwo")
-    {
-      drawNum = 2;
-    }
-    else if (li == "drawfour")
-    {
-      drawNum = 4;
-    }
-    else
-    {
-      drawNum = 0;
+    if (d2 && card == "drawtwo") {
+      d2++;
+      put[turn]++;
+      proceed();
+      continue;
     }
 
-    cout << "i = " << i << ", drawNumBefore = " << drawNumBefore << ", drawNum = " << drawNum << ", turn = " << turn << ", card = " << li << ", drawCardCnt = " << drawCardCnt << endl;
-
-    if (i == 0 || drawNumBefore == drawNum)
-    {
-      c[turn]++;
-      if (li == "number")
-      {
-        turn = nextTurn(turn, d, n);
-      }
-      else if (li == "reverse")
-      {
-        d = -d;
-        turn = nextTurn(turn, d, n);
-      }
-      else if (li == "skip")
-      {
-        turn = nextTurn(turn, d, n);
-        turn = nextTurn(turn, d, n);
-      }
-      else
-      {
-        turn = nextTurn(turn, d, n);
-      }
-      drawCardCnt++;
-    }
-    else
-    {
-      c[turn] += drawNumBefore * drawCardCnt;
-      turn = nextTurn(turn, d, n);
-      c[turn]++;
-      if (li == "number")
-      {
-        turn = nextTurn(turn, d, n);
-      }
-      else if (li == "reverse")
-      {
-        d = -d;
-        turn = nextTurn(turn, d, n);
-      }
-      else if (li == "skip")
-      {
-        turn = nextTurn(turn, d, n);
-        turn = nextTurn(turn, d, n);
-      }
-      else
-      {
-        turn = nextTurn(turn, d, n);
-      }
-
-      drawCardCnt = 1;
+    if (d4 && card == "drawfour") {
+      d4++;
+      put[turn]++;
+      proceed();
+      continue;
     }
 
-    cout << c[0];
-    for (int k = 1; k < n; k++) {
-      cout << " " << c[k];
+    if(d2) {
+      draw[turn] += 2 * d2;
+      d2 = 0;
+      proceed();
     }
-    cout << endl;
 
-    drawNumBefore = drawNum;
+    if(d4) {
+      draw[turn] += 4 * d4;
+      d4 = 0;
+      proceed();
+    }
+
+    put[turn]++;
+
+    if (card == "drawtwo") {
+      d2++;
+    } else if (card == "drawfour") {
+      d4++;
+    } else if (card == "skip") {
+      proceed();
+    } else if (card == "reverse") {
+      dir *= -1;
+    } else if (card == "number") {
+      // do nothing.
+    }
+    proceed();
   }
 
-  int max = 0;
-  int maxId = -1;
-  for (int i = 0; i < n; i++)
-  {
-    cout << c[i] << " ";
-    if (c[i] > max)
-    {
-      max = c[i];
-      maxId = i;
-    }
-  }
-  cout << endl;
-  cout << maxId + 1 << " " << max << endl;
+  turn = (turn - dir + n) % n;
+
+  cout << turn + 1 << " " << put[turn] - draw[turn] << endl;
+
+  return 0;
 }
