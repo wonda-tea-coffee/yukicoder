233a59829be541192383d8bcab0233320b4c0367
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-29 19:10:18 +0900

    solve

---
+++ 240.cpp
@@ -0,0 +1,86 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+#define pb              push_back
+#define fst             first
+#define int             long long
+#define INF             __LONG_LONG_MAX__
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {-2,-2,-1,-1, 1,1,2, 2};
+const int dy[8] = {-1, 1, 2,-2,-2,2,1,-1};
+
+void solve() {
+  ll x, y; cin >> x >> y;
+  queue<ll> qx, qy, qc;
+  qx.push(0); qy.push(0); qc.push(0);
+
+  while (!qx.empty()) {
+    ll xi = qx.front(); qx.pop();
+    ll yi = qy.front(); qy.pop();
+    ll ci = qc.front(); qc.pop();
+    // debug3(xi, yi, ci);
+
+    if (xi == x && yi == y) {
+      YES();
+      return;
+    }
+    if (ci == 3) continue;
+
+    for (int i = 0; i < 8; i++) {
+      int nx = xi + dx[i];
+      int ny = yi + dy[i];
+      qx.push(nx);
+      qy.push(ny);
+      qc.push(ci + 1);
+    }
+  }
+  NO();
+}
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}
